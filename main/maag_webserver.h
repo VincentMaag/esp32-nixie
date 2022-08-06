d41410cd521b98e398024667cf03029df805be84
Vincent Maag <vincentmaag@gmx.ch>
2022-08-06 17:08:13 +0200

    MaagWifi class
    createt MaagWifi class, create maagWifi instance in app_main, connection possible in ap and sta modes and we can set wifi parameters as we wish (ip, ssid, etc.)

---
+++ main/maag_webserver.h
@@ -0,0 +1,11 @@
+/*
+    ...
+
+*/
+#ifndef __MAAG_WEBSERVER_H__
+#define __MAAG_WEBSERVER_H__
+
+
+
+
+#endif /* __MAAG_WEBSERVER_H__ */
--- main/main.cpp
+++ main/main.cpp
@@ -2,49 +2,65 @@
 #include <string.h>
 #include <string>
 
-
-#include "esp_log.h"
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
 
-static const char *TAG = "Main";
+#include "nixie_projdefs.h"
+#include "maag_wifi.h"
 
-class Juice{
-    public:
-        int iNumber;
-        std::string sWhatItDoes;
-
-        void getWhatItDoes(){
-            ESP_LOGI(TAG, "Number %i %s", iNumber, sWhatItDoes.c_str());
-        }
-        void getWhatItWants(){
-            ESP_LOGE(TAG, "%i Wants chocolate", iNumber);
-        }
-};
-
-
-
+static const char *TAG = "main";
 
 extern "C" void app_main()
 {
+    ESP_LOGI(TAG, "STARTING MAIN");
+    // =====================================================================
+    // global initializing, objects, parameters, nvs, etc.
+    //
+    ESP_LOGI(TAG, "Initializing nvs");
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    //
+    MaagWifi maagWifi;
 
-    Juice myJuice1;
-    Juice myJuice2;
+    maagWifi.setIP("192.168.178.140");
+    maagWifi.setGW("192.168.178.1");
+    maagWifi.setSSID("FRITZ!Box 5490 WT");
+    maagWifi.setPW("55940362741817360715");
 
-    myJuice1.iNumber = 1;
-    myJuice1.sWhatItDoes = "It makes your head explode";
+    // maagWifi.init_ap();
+    maagWifi.init_sta();
 
-    myJuice2.iNumber = 2;
-    myJuice2.sWhatItDoes = "hahahahahahahaaa, blblblblblb-hahahaha";
+    // =====================================================================
+    // create all user tasks
+    ESP_LOGI(TAG, "Creating some Tasks");
+    // wifi
+    //xTaskCreatePinnedToCore(maag_wifi_task, "maag_wifi_task", 4096, (void *)0, 5, NULL, 0);
+    // ...
 
 
-    while (1)
+    while (true)
     {
 
-        myJuice1.getWhatItDoes();
+        if (maagWifi.getConnectionStatus() == false)
+        {
+            ESP_LOGE(TAG, "caught disconnected esp");
+            maagWifi.wifi_try_connect_sta();
+        }
+
         vTaskDelay((1000 / portTICK_PERIOD_MS));
-        myJuice2.getWhatItDoes();
-        vTaskDelay((1000 / portTICK_PERIOD_MS));
-        
     }
+
+    ESP_LOGI(TAG, "FINISHED MAIN");
 }
